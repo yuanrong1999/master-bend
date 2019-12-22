5461cc20179bf50d9a3ad2837d9e7c85818c3398
yuanrong1999 <55180626+yuanrong1999@users.noreply.github.com>
2019-12-22 16:49:19 +0800

    影片按鈕

--- Assets/Sprites/VidPlayer.cs
+++ Assets/Sprites/VidPlayer.cs
@@ -1,26 +1,99 @@
-﻿using UnityEngine.Video;
+﻿using System.Collections.Generic;
+using UnityEngine.Video;
 using UnityEngine;
+using System;
 
 public class VidPlayer : MonoBehaviour
 {
     VideoPlayer a = new VideoPlayer();
+    string ButtonText = "Skip";
+    bool SkipORnot = false; //Skip不作用
+    public List<VideoClip> videos = new List<VideoClip>();
+    public Transform skip;
+    //System.Collections.Generic.IList<VideoClip> videos = new IList<VideoClip>()
+
 
     private void Start()
     {
-       a = GetComponent<VideoPlayer>();
-        a.playOnAwake = false;
+        a = GetComponent<VideoPlayer>();
+        //a.clip;
+        a.loopPointReached += EndReached;
+        gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>("pixiv頭貼");
+
+        //結束程式
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
 
     }
 
-    private void OnGUI()
+    void EndReached(UnityEngine.Video.VideoPlayer a)
+    {
+        a.Stop();//停止目前影片
+        a.clip = videos[1];//播放陣列1影片
+        a.Play();
+        a.Stop();//停止目前影片
+
+        
+        Input.GetKeyDown("mouse0");
+
+        a.clip = videos[2];//播放陣列2影片
+        a.Play();
+        a.Stop();//停止目前影片
+        a.clip = videos[3];//播放陣列3影片
+        a.Play();
+        a.Stop();
+        Application.Quit();
+    }
+    /*private void OnGUI()
     {
         if (GUILayout.Button("Skip"))
         {
+            a.Stop();//停止目前影片
+            a.clip = videos[2];//播放陣列2影片
             a.Play();
         }
 
+        else if (GUILayout.Button("Skip2"))
+        {
+            a.Stop();//停止目前影片
+            a.clip = videos[3];//播放陣列3影片
+            a.Play();
+        }
+        */
 
-        
+
     }
 
+
+
+internal class waitforseconds
+{
+    public waitforseconds()
+    {
+    }
 }
+
+/*private void OnGUI()
+{
+    if (Input.GetKeyDown("Mouse0"))
+    {
+        a.Stop();//停止目前影片
+        a.clip = videos[2];//播放陣列2影片
+        a.Play();
+    }
+}*/
+
+/**if (GUI.Button(new Rect(300,0,0,0),ButtonText))
+{
+    if(SkipORnot==false)
+    {
+    SkipORnot = true;
+    }
+}**/
+
+
+
+
+
---
+++ Assets/Sprites/jpg.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jpg : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>
+
+        ("pixiv頭貼");
+    }
+
+}
